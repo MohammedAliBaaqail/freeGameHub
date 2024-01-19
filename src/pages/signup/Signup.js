0e0c1723cb7309b6aab2075e9ebccda475a3b0cb
Mohammed Ali <mohammed.ali.baaqail@gmail.com>
2024-01-19 21:54:31 +0300

    added sorting for cat pages

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -10,12 +10,25 @@ const Signup = () => {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const { signup, error, isLoading } = useSignup();
   const { user } = useSelector((state) => state.user);
   const [signupSuccess, setSignupSuccess] = useState(false);
+  const [passwordMismatch, setPasswordMismatch] = useState(false); // New state for password mismatch
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // Check if password and confirmed password match
+    if (password !== confirmPassword) {
+      // Handle password mismatch
+      console.error("Password and confirmed password do not match");
+      setPasswordMismatch(true);
+      setSignupSuccess(false);
+      return;
+    } else {
+      setPasswordMismatch(false);
+    }
 
     try {
       await signup({ email, username, password });
@@ -34,6 +47,9 @@ const Signup = () => {
         <Input type={"text"} onChange={(e) => setUsername(e.target.value)} value={username} name={"Username"} />
         <Input type={"email"} onChange={(e) => setEmail(e.target.value)} value={email} name={"Email"} />
         <Input type={"password"} onChange={(e) => setPassword(e.target.value)} value={password} name={"Password"} />
+        <Input type={"password"} onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} name={"Confirm Password"} />
+
+       
 
         {isLoading ? (
           <div className="loading-favourite">
@@ -48,7 +64,8 @@ const Signup = () => {
           <>
             <Button text={"Sign up"} disabled={isLoading} />
             {signupSuccess && !error && <div className="success-message">Signup successful! Check your email inbox and spam box for verification instructions.</div>}
-            {error && <div className="error">{error}</div>}
+            { error && <div className="error">{error}</div>}
+            {passwordMismatch && <div className="error">Password and confirmed password do not match.</div>}
           </>
         )}
       </form>

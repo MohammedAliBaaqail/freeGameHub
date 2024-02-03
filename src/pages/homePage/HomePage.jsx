418d0195d5dd87043231c77f43145a6ae599b346
Mohammed Ali <mohammed.ali.baaqail@gmail.com>
2024-02-03 13:37:51 +0300

    ..

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -50,7 +50,7 @@ export const HomePage = () => {
           />
         </div>
       ));
-console.log(oneSection)
+
     return (
       <Section
         key={oneSection.title}
@@ -63,7 +63,7 @@ console.log(oneSection)
       />
     );
   });
-console.log(allSections)
+
   return (
     <div className="home-page" {...animations}>
       <div className="hero-section">

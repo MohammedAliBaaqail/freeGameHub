2049a9f901ab3a66e4d04dce16a5a038dad33f65
Mohammed Ali <mohammed.ali.baaqail@gmail.com>
2024-02-06 17:25:55 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+import { Analytics } from '@vercel/analytics/react';
+
 import { BrowserRouter  } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './app/store'
@@ -14,6 +16,7 @@ root.render(
     <BrowserRouter>
     <Provider store={store} >
     <App />
+    <Analytics />
     </Provider>
     </BrowserRouter>
   </React.StrictMode>

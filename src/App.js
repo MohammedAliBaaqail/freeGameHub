00a6e7e68e926f068565caecb8a21e7435b5931f
oMaaBo <mohammed.ali.baaqail@gmail.com>
2025-01-18 16:28:24 +0300

    .

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import useArabicStyles from './hooks/useArabicStyles';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 
+
 function App() {
   useArabicStyles();
   const { user } = useSelector((state) => state.user);
